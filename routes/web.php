996178cd1112d0e36f0972986a3cf4b521bef790
justputoff <kholid.hasibuan35@gmail.com>
2024-08-02 04:20:48 +0700

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -14,11 +14,11 @@ use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', [LandingController::class, 'index'])->name('landing');
-Route::get('projects/list', [ProjectController::class, 'projectList'])->name('projects.projectList');
 
 Route::get('/dashboard', [DashboardController::class, 'index'])->middleware(['auth', 'verified'])->name('dashboard');
 
 Route::middleware('auth')->group(function () {
+    Route::get('projects/list', [ProjectController::class, 'projectList'])->name('projects.projectList');
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
