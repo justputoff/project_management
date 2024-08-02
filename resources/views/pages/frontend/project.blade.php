55b8aecc59cda0fbfd79c13dbc1baa9d146a8f93
justputoff <kholid.hasibuan35@gmail.com>
2024-08-02 15:55:01 +0700

    Update project.blade.php

--- resources/views/pages/frontend/project.blade.php
+++ resources/views/pages/frontend/project.blade.php
@@ -6,7 +6,7 @@
         <div class="col-lg-12">
             <div class="card shadow-sm">
                 <div class="card-body">
-                    <div class="description-container bg-white text-dark" style="max-height: 600px; overflow-y: auto;">
+                    <div class="description-container bg-white text-dark" style="max-height: 1080px; overflow-y: auto;">
                         <p class="mt-3 description text-dark bg-white">{!! $project->description !!}</p>
                     </div>
                 </div>
@@ -29,7 +29,7 @@
       }
 
       .description-container img {
-        max-width: max-content;
+        max-width: 720px;
       }
 
       .description-container figcaption{
