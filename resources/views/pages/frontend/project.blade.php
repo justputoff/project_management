e0e08fcf31a8b144662ab5586826bc0ef15c78b8
justputoff <kholid.hasibuan35@gmail.com>
2024-08-02 15:48:03 +0700

    Update project.blade.php

--- resources/views/pages/frontend/project.blade.php
+++ resources/views/pages/frontend/project.blade.php
@@ -1,9 +1,31 @@
 @extends('layouts.frontend.main')
 
 @section('content')
-
-<div class="container">
-    <p>{!! $project->description !!}</p>
+<div class="container mt-4">
+    <div class="row justify-content-center">
+        <div class="col-lg-8">
+            <div class="card shadow-sm">
+                <div class="card-body">
+                    <div class="project-description overflow-hidden">
+                        {!! $project->description !!}
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
 </div>
-
 @endsection
+
+@push('styles')
+<style>
+    .project-description {
+        word-wrap: break-word;
+        overflow-wrap: break-word;
+        max-width: 100%;
+    }
+    .project-description img {
+        max-width: 100%;
+        height: auto;
+    }
+</style>
+@endpush
