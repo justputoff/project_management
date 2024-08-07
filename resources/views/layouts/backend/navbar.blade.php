40fd5f16074a44d4bd4291c2e1d165beb3e52244
justputoff <kholid.hasibuan35@gmail.com>
2024-08-07 23:14:23 +0700

    Update navbar.blade.php

--- resources/views/layouts/backend/navbar.blade.php
+++ resources/views/layouts/backend/navbar.blade.php
@@ -33,8 +33,8 @@
               </li>
 
               <!-- Project List -->
-              <li class="menu-item {{ Route::is('projects.list') ? 'active' : '' }}">
-                <a href="{{ route('projects.list') }}" class="menu-link">
+              <li class="menu-item {{ Route::is('project.projectList') ? 'active' : '' }}">
+                <a href="{{ route('project.projectList') }}" class="menu-link">
                   <i class="menu-icon tf-icons bx bxs-book"></i>
                   <div data-i18n="Project List">Project List</div>
                 </a>
